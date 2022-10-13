0f5a072e188a7d6b9687423fe72702c62d6c94f1
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-10-12 21:00:26 -0300

    feat: add optios size

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -6,6 +6,15 @@ export default {
     component: Text,
     args: {
         children: "Lorem Ipsum",
+        size: 'md',
+    },
+    argTypes: {
+        size: {
+            options: ['sm','md','lg'],
+            control: {
+                type: 'inline-radio'
+            }
+        }
     }
 } as Meta<TextProps>
 
@@ -29,5 +38,12 @@ export const CustomComponent: StoryObj<TextProps> = {
         children: (
             <p>testando</p>
         )
-    }
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true,
+            }
+        }
+    },
 }
